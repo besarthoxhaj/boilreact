b2433cacbabda7e46347789d6e75300d3b7c3ed8
besart <besartshyti@gmail.com>
2017-05-06 15:25:20 +0100

    [feature] add home constants

--- app/constants.js
+++ app/constants.js
@@ -2,8 +2,10 @@
 
 import * as alertConst from './containers/Alert/constants';
 import * as modalConst from './containers/Modal/constants';
+import * as homeConst from './containers/Home/constants';
 
 export default {
   ...alertConst,
   ...modalConst,
+  ...homeConst,
 };
