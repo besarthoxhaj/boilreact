79a1fcb1c1bacaae5abafbbcd299a8ca494ce142
besart <besartshyti@gmail.com>
2017-05-06 15:19:46 +0100

    [feature] add general constants

---
+++ app/constants.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+import * as alertConst from './containers/Alert/constants';
+import * as modalConst from './containers/Modal/constants';
+
+export default {
+  ...alertConst,
+  ...modalConst,
+};
